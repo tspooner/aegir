380678c1aa58008fd03620941b0d78e5333f2bdc
Tom Spooner <spooner10000@gmail.com>
2023-01-02 15:13:25 +0000

    feat(meta): implement minimal Differentiable for Cached

--- aegir/src/meta/cached.rs
+++ aegir/src/meta/cached.rs
@@ -3,6 +3,7 @@ use crate::{
     Write,
     Contains,
     Function,
+    Differentiable,
     Identifier,
     AegirResult,
 };
@@ -47,5 +48,20 @@ where
         ctx.as_mut().write(self.ident, value.clone());
 
         Ok(value)
+    }
+}
+
+impl<N, T> Differentiable<T> for Cached<N, T>
+where
+    N: Differentiable<T>,
+    T: Identifier,
+{
+    type Adjoint = N::Adjoint;
+
+    fn adjoint(&self, target: T) -> Self::Adjoint {
+        // TODO - find a way to propagate the cached terms through the derivative.
+        // This will require some kind of search in which find-and-replace all instances of
+        // N in Self::Adjoint with Cached<N, T>.
+        self.node.adjoint(target)
     }
 }
